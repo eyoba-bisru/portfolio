cd111efaad61b691dfef4a9985130e6ff827e1de
Eyob <eabebe91@gmail.com>
2023-07-18 13:01:49 +0300

    shadcn ui added and home finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import SocialLink from "@/components/social-link";
 import Image from "next/image";
-import Link from "next/link";
 import home from "../data/home.json";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div className="xs:max-w-2xl lg:max-w-4xl mx-auto sm:px-4 xs:px-2">
         <header className="flex justify-between items-center mt-5" id="#home">
-          <div className="w-12 h-12 relative">
+          <div className="w-8 h-8 relative">
             <Image
               src="/eyob-logo.png"
               alt="logo"
@@ -17,7 +18,7 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex items-center gap-4 font-semibold">
+          <div className="flex items-center gap-4 font-semibold text-sm">
             <a href="#skills">Skills</a>
             <a href="#works">Works</a>
           </div>
@@ -38,16 +39,34 @@ export default function Home() {
           </div>
         </header>
         <main>
-          <div className="mx-auto flex flex-col justify-center items-center xs:max-w-lg lg:max-w-xl mt-12">
-            <h1 className="font-extrabold text-5xl">{home["title"]}</h1>
-            <h2 className="mt-8 font-bold text-3xl bg-black text-white">
+          <div className="mx-auto flex flex-col justify-start mt-20 items-center xs:max-w-lg lg:max-w-xl h-[calc(100vh-10rem)]">
+            <h1 className="font-extrabold text-4xl">{home["title"]}</h1>
+            <h2 className="mt-10 font-bold text-2xl bg-black text-white">
               {home["greet"]}
             </h2>
             <p
-              className="mt-4 text-lg text-center"
+              className="mt-8 text-md text-center text-gray-700"
               dangerouslySetInnerHTML={{ __html: home["description"] }}
             />
+            <div className="mt-20 flex items-center gap-8">
+              <a
+                download
+                href="/docs/resume.pdf"
+                className={buttonVariants({ variant: "default" })}
+              >
+                Download resume
+              </a>
+              <a
+                href="#works"
+                className={buttonVariants({ variant: "outline" })}
+              >
+                Check my work
+              </a>
+            </div>
           </div>
+          {/* <div className="mt-10 text-3xl font-extrabold h-[100vh]" id="skills">
+            <header className="text-center">Skills</header>
+          </div> */}
         </main>
       </div>
     </div>
--- src/components/social-link.tsx
+++ src/components/social-link.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function SocialLink({ image: src, link }: Props) {
   return (
-    <div className="w-7 h-7 relative">
+    <div className="w-5 h-5 relative">
       <a href={link} target="_blank">
         <Image className="object-cover" src={src} fill alt="telegram" />
       </a>
