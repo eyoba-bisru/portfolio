ddadec127360516c87efcd77e2c7d95a2eec508d
Eyob <eabebe91@gmail.com>
2023-07-19 16:59:18 +0300

    key added

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -19,24 +19,10 @@ export default function Navbar() {
         <a href="#works">Works</a>
       </div>
 
-<<<<<<< HEAD
-      <div className="flex items-center gap-6">
-        <div className="flex items-center gap-2">
-          {links.map((link) => (
-            <SocialLink
-              key={link.img}
-              image={theme === "light" ? link.img : link["img-dark"]}
-              link={link.link}
-            />
-          ))}
-        </div>
-        <ModeToggle />
-=======
       <div className="flex items-center gap-2">
         {links.map((link) => (
-          <SocialLink image={link.img} link={link.link} />
+          <SocialLink key={link.img} image={link.img} link={link.link} />
         ))}
->>>>>>> parent of 77d787c (dark mode added)
       </div>
     </nav>
   );
